f343ce503dc8d52ba17e093942139fd804b7b949
Дима Лишухай <dlishuhai@yandex.ru>
2022-12-03 09:57:00 +0300

    permission changes

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,4 +8,4 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or obj.author.role == 'admin')
+                or request.user.is_admin or request.user.is_superuser)
